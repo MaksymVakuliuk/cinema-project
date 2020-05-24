9688bbf3430303105212fb2acb4f1fb43918e16f
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-05-24 20:13:32 +0300

    HashUtil, AuthenticationException, AuthenticationServise, User model, UserService implemented. UserDao created.

---
+++ src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.dao.UserDao;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.UserService;
+import com.dev.cinema.util.HashUtil;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    public User add(User user) {
+        byte[] salt = HashUtil.getSalt();
+        user.setSalt(salt);
+        user.setPassword(HashUtil.hashPassword(user.getPassword(), salt));
+        return userDao.add(user);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+}
